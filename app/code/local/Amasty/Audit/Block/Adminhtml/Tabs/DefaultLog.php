5eba63da2176c9326469c56586726a01f80b86bd
sickdaflip <philippbreitsprecher@gmail.com>
2020-02-26 11:49:11 +0100

    first upload

---
+++ app/code/local/Amasty/Audit/Block/Adminhtml/Tabs/DefaultLog.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2020 Amasty (https://www.amasty.com)
+ * @package Amasty_Audit
+ */
+
+
+abstract class Amasty_Audit_Block_Adminhtml_Tabs_DefaultLog extends Amasty_Audit_Block_Adminhtml_DefaultLog
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setUseAjax(true);
+    }
+}
